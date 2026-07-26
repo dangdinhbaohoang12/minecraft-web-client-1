5b714eed41ad1cccaabd3b302a0631defefedc39
dangdinhbaohoang12 <hoang.s297312@ilamail.edu.vn>
2026-07-26 11:16:42 +0700

    Update controls.ts

--- src/controls.ts
+++ src/controls.ts
@@ -315,7 +315,8 @@ const setSneaking = (state: boolean) => {
   gameAdditionalState.isSneaking = state
   bot.setControlState('sneak', state)
 
-  if (state && (bot as any).vehicle) {
+  const vehicle = (bot as any).vehicle
+  if (state && vehicle) {
     try {
       bot.dismount()
     } catch {}
